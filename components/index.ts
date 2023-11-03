37ac2e527b9bcda22af4bfadb5b215a6e158f0b8
anasmunawar2 <anasmunawar2@gmail.com>
2023-11-04 04:47:13 +0500

    first commit

---
+++ components/index.ts
@@ -0,0 +1,25 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import SearchButton from "./SearchButton";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  ShowMore,
+  SearchBar,
+  CustomFilter,
+  CarDetails,
+  SearchButton,
+  SearchManufacturer,
+  CarCard,
+};
